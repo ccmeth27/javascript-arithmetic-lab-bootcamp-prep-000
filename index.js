6e54f9767bc16153786e588aa48fd0f7c6df2c34
Connor Meth <connormeth27@gmail.com>
2019-08-04 16:07:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function add(a, b) {
 console.log(add(1, 80));
 
 function subtract(a, b) {
-  return (a + b);
+  return (a - b);
 }
 console.log(subtract(60, 40));
+
+function multiply(a, b) {
+  return (a * b);
+}
+console.log(multiply(2, 3.4));
+
+function divide(a, b) {
+  return (a / b);
+}
+console.log(divide(5.0, 2.5));
